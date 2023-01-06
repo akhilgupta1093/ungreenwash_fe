c0153cfa6bc897c752fbe345ad9ec372d58baaa0
Akhil Gupta <akhilgupta@akhils-air.lan>
2023-01-06 13:04:30 -0800

    using the development flag

--- src/apis/routes.ts
+++ src/apis/routes.ts
@@ -1,4 +1,4 @@
-const local = false
+const local = process.env.NODE_ENV === "development";
 export const domain = local ? "http://localhost:8000" : "https://ugw-backend.herokuapp.com";
 
 export function getURL(path: string) {
